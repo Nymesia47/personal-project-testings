cafab243c99ba464338248b94df9182c124cd007
Gabriella Calvano <gabcalvano@gmail.com>
2024-12-28 21:07:41 +0100

    initial commit

---
+++ main.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const createBtn = document.querySelector('.js-btn');
+
+function createMealPlan () {
+    console.log('I got clicked');
+};
+
+createBtn.addEventListener('click', createMealPlan);
